e7c7b459bef3678877fe06a6ace0d3a2fa3c003e
martinspadrna <martinspadrna@gmail.com>
2026-07-05 03:13:11 +0200

    oprava nad panelem

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'domacnost-plus-';
-const CACHE_NAME = `${CACHE_PREFIX}v0-1-375`;
+const CACHE_NAME = `${CACHE_PREFIX}v0-1-376`;
 const APP_ASSETS = [
   './',
   './index.html',
