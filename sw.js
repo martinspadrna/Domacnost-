8e8ec9ec0b2cefc009e4ee7046077aab11df8759
martinspadrna <martinspadrna@gmail.com>
2026-07-12 18:49:36 +0200

    442

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'domacnost-plus-';
-const CACHE_NAME = `${CACHE_PREFIX}v0-1-441`;
+const CACHE_NAME = `${CACHE_PREFIX}v0-1-442`;
 const APP_ASSETS = [
   './',
   './index.html',
